602c6d5662d00226d8d656395e6d2931f7627376
TeresaHS <teresahernandezsanchez.guadalupe@alumnado.fundacionloyola.net>
2026-03-23 13:42:24 +0100

    Add files via upload

---
+++ TAREA_23_02_2026/SQL/insercionMasiva.sql
@@ -0,0 +1,11 @@
+USE user1daw_BD1-16;
+
+
+INSERT INTO alumnos (puesto, nombre) VALUES ('00', 'Manuel Amigo Royán');
+INSERT INTO alumnos (puesto, nombre) VALUES ('01', 'Samuel Barril Sosa');
+INSERT INTO alumnos (puesto, nombre) VALUES ('02', 'Yehú Blanco Pinillo');
+INSERT INTO alumnos (puesto, nombre) VALUES ('13', 'Aitor Gragera Martin');
+INSERT INTO alumnos (puesto, nombre) VALUES ('14', 'Ivan Rodríguez Gómez-Landero');
+INSERT INTO alumnos (puesto, nombre) VALUES ('15', 'Teresa Hernández Sánchez');
+INSERT INTO alumnos (puesto, nombre) VALUES ('20', 'Sara Rivero Caminero');
+INSERT INTO alumnos (puesto, nombre) VALUES ('21', 'Carlos Colorado Anselmo');
